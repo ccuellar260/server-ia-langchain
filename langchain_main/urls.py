ce95fa7c82e2cccf08677036515aaa4bd3f9e77c
Cristian Cuellar <ccuellar260@gmail.com>
2024-11-28 17:03:38 -0400

    xd

--- langchain_main/urls.py
+++ langchain_main/urls.py
@@ -5,7 +5,7 @@ from langchain_app.controllers import LangchainController
 from langchain_app.controllers import LangchainAgentController
 
 urlpatterns = [
-    # path('admin/', admin.site.urls),
+    path('/', admin.site.urls),
     # path('api/', admin.site.urls),
     path('api/hola', LangchainAgentController.hola),
     path('api/consulta_ia', LangchainController.langhchain_get),
